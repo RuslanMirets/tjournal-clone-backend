dcfb619589af971ec6a6941cfc4e2503c10ad2db
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-09 16:23:43 +0300

    Урок №6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { UserEntity } from './user/entities/user.entity';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'tjournal',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
