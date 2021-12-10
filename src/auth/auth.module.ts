5d6668bedf9c89155c42b83cfae70b3161965514
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-10 15:31:59 +0300

    Урок №10

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,22 @@
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'test',
+      signOptions: { expiresIn: '100y' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
